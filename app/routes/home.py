a42e321e1f6a7dde38e4d45aba6eff992ac03170
hejazy <hegazy@outlook.com>
2023-08-25 01:56:39 +0300

    FInalize the routes

---
+++ app/routes/home.py
@@ -0,0 +1,11 @@
+from flask import render_template, request
+from __main__ import app
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    devices = load_devices_from_excel()
+    search_query = request.args.get('search', '')
+    if search_query:
+        devices_filtered = [device for device in devices if search_query.lower() in device['hostname'].lower()]
+        return render_template('home.html', devices=devices_filtered, search_query=search_query)
+    return render_template('home.html', devices=devices, search_query='')
