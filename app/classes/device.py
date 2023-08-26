12996807dd004ee511fbb5b51e86e62dcca157d3
hejazy <hegazy@outlook.com>
2023-08-26 20:46:33 +0300

    Format

--- app/classes/device.py
+++ app/classes/device.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class DeviceForm(FlaskForm):
-    hostname = StringField('Hostname', validators=[DataRequired()])
-    ip_address = StringField('IP Address', validators=[DataRequired()])
-    port = StringField('Port', validators=[DataRequired()])
-    switch = StringField('Switch', validators=[DataRequired()])
-    submit = SubmitField('Add Device')
+    hostname = StringField("Hostname", validators=[DataRequired()])
+    ip_address = StringField("IP Address", validators=[DataRequired()])
+    port = StringField("Port", validators=[DataRequired()])
+    switch = StringField("Switch", validators=[DataRequired()])
+    submit = SubmitField("Add Device")
